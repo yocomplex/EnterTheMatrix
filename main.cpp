2e1a49e2406d19062d64e7ce6fcc54140446f23c
Joshua Zamora <95387697+yocomplex@users.noreply.github.com>
2023-07-17 05:59:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
         return 0;
     }
     std::cout << "Enter the Matrix!" << std::endl;
-    // srand(atoi(argv[3])); // could use strol for error checking but yolo
-    // LinkedList theMatrix(atoi(argv[1]), atoi(argv[2]));
-    // theMatrix.print();
-    // theMatrix.traceTheCall(0);
-    // theMatrix.traceTheCall(1);
+    srand(atoi(argv[3])); // could use strol for error checking but yolo
+    LinkedList theMatrix(atoi(argv[1]), atoi(argv[2]));
+    theMatrix.print();
+    theMatrix.traceTheCall(0);
+    theMatrix.traceTheCall(1);
 
     // //Some Test Cases To Help Code It
     srand(26); //you always get the same random numbers.
@@ -22,22 +22,22 @@ int main(int argc, char *argv[])
     list.traceTheCall(0);
     list.traceTheCall(1);
     list.traceTheCall(5);
-    // LinkedList list0(10, 20);
-    // list0.traceTheCall(0);
-    // list0.traceTheCall(1);
-    // list0.print();
-    // LinkedList list1(0, 0);
-    // LinkedList list2(0, 1);
-    // LinkedList list3(1, 0);
-    // LinkedList list4(1, 1);
-    // list1.print();
-    // list1.traceTheCall(0);
-    // list2.print();
-    // list2.traceTheCall(0);
-    // list3.print();
-    // list3.traceTheCall(0);
-    // list4.print();
-    // list4.traceTheCall(0);
+    LinkedList list0(10, 20);
+    list0.traceTheCall(0);
+    list0.traceTheCall(1);
+    list0.print();
+    LinkedList list1(0, 0);
+    LinkedList list2(0, 1);
+    LinkedList list3(1, 0);
+    LinkedList list4(1, 1);
+    list1.print();
+    list1.traceTheCall(0);
+    list2.print();
+    list2.traceTheCall(0);
+    list3.print();
+    list3.traceTheCall(0);
+    list4.print();
+    list4.traceTheCall(0);
 
     return 0;
 }
